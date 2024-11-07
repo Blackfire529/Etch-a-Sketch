cc4762ba5859ad1ca095df6ea0aaaa296bf20415
Blackfire529 <167809855+Blackfire529@users.noreply.github.com>
2024-11-07 10:25:37 -0600

    Fixed_the_grid_to_be_correctly_16x16

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
 const container = document.getElementsByClassName('container');
 console.log(container.length);
 // a loop to make the all the div for the 16 x 16 grid
-for (let i = 0; i < 256; i++) {
-  let div = document.createElement('div');
-  div.classList.add('grid-item');
-  container[0].appendChild(div);
+for (let i = 0; i < 16; i++) {
+  let row = document.createElement('div');
+  row.classList.add(`row`);
+  for (let i = 0; i < 16; i++) {
+    let div = document.createElement('div');
+    div.classList.add(`grid-item`);
+    row.appendChild(div);
+  }
+  container[0].appendChild(row);
 }
+
+
